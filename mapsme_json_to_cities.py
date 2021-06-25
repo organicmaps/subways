b422ccb6adb8d2b374a4277f46e00bd520e49495
Alexey Zakharenkov <a-zakh@yandex.ru>
2021-06-26 00:41:11 +0300

    Use argparse module in mapsme_json_to_cities.py

--- mapsme_json_to_cities.py
+++ mapsme_json_to_cities.py
@@ -1,30 +1,41 @@
-"""
-This script generates a list of good/all network names.
-It is used by subway render to generate the list of network at frontend.
-It uses two sources: a mapsme.json validator output with good networks, and
-a google spreadsheet with networks for the process_subways.download_cities()
-function.
-"""
-
+import argparse
 import json
-import sys
 
 from process_subways import download_cities
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: {} <mapsme.json> [--with-bad]'.format(sys.argv[0]))
-        sys.exit(1)
+    arg_parser = argparse.ArgumentParser(
+        description="""
+            This script generates a list of good/all network names.
+            It is used by subway render to generate the list of network at frontend.
+            It uses two sources: a mapsme.json validator output with good networks, and
+            a google spreadsheet with networks for the process_subways.download_cities()
+            function.""",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
 
-    with_bad = len(sys.argv) > 2 and sys.argv[2] == '--with-bad'
+    arg_parser.add_argument(
+        'subway_json_file',
+        type=argparse.FileType('r'),
+        help="Validator output defined by -o option of process_subways.py script"
+    )
 
-    subway_json_file = sys.argv[1]
-    with open(subway_json_file) as f:
-        subway_json = json.load(f)
+    arg_parser.add_argument(
+        '--with-bad',
+        action="store_true",
+        help="Whether to include cities validation of which was failed",
+    )
+
+    args = arg_parser.parse_args()
+
+    with_bad = args.with_bad
+    subway_json_file = args.subway_json_file
+    subway_json = json.load(subway_json_file)
+
     good_cities = set(n.get('network', n.get('title')) for n in subway_json['networks'])
-
     cities = download_cities()
+
     lines = []
     for c in cities:
         if c.name in good_cities:
