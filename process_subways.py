06fc8bc13cc7d23239808cddd1ef9ae57b22c100
Alexander Borsuk <me@alex.bio>
2022-10-07 23:21:08 +0200

    Force IPv4 usage

--- process_subways.py
+++ process_subways.py
@@ -25,6 +25,17 @@ from subway_structure import (
     MODES_RAPID,
 )
 
+# Hack to always use IPv4.
+import socket
+
+old_getaddrinfo = socket.getaddrinfo
+def new_getaddrinfo(*args, **kwargs):
+    responses = old_getaddrinfo(*args, **kwargs)
+    return [response
+            for response in responses
+            if response[0] == socket.AF_INET]
+socket.getaddrinfo = new_getaddrinfo
+
 
 def overpass_request(overground, overpass_api, bboxes):
     query = '[out:json][timeout:1000];('
